85f7edeb7f0dc736fd2ac5f6c2f560fcb713b59a
Thrillophilia <adijr9487@gmail.com>
2023-02-14 18:58:40 +0530

    :hammer: added loading spinner

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React from "react"
-import './App.css';
-import Product from "./components/Product"
+import React from "react";
+import "./App.css";
+import Product from "./components/Product";
+import Loader from "./utils/loader/loader";
 // import products from "./data/product.js"
 function App() {
-
-  const [products, setProducts] = React.useState([])
+  const [products, setProducts] = React.useState(null);
 
   React.useEffect(() => {
-    
     fetch("https://dummyjson.com/products")
       .then((response) => response.json())
       .then((data) => {
-          setProducts(data.products)
+        setProducts(data.products);
       })
       .catch((error) => console.log(error));
-  
   }, []);
 
-  return (
-    <div className="App">
-      {products.length > 0 && products.map((product) => (
+  const product = () => {
+    if (products === null) {
+      return <Loader />;
+    } else {
+      return products.map((product) => (
         <Product key={product.id} product={product} />
-      ))}
-    </div>
-  );
+      ));
+    }
+  };
+
+  return <div className="App">{product()}</div>;
 }
 
 export default App;
